68e1430e76cbd833e059583c70f50a4c4d6d2f42
tishisaka <tatsuya.ishisaka@mail.rakuten.com>
2013-06-06 01:15:12 -0400

    implement ISBN module

---
+++ config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+/**
+ * This is the config for the study book
+ *
+ */
+
+/**
+ * Constructor for the search configuration object
+ *
+ * @class fc.modules.search.config
+ * @public
+ */
+
+module.exports = {
+    'isbn': {
+        host:'isbndb.com',
+        path:'/api/v2/json/2DAJ7GKP/books?q='
+    }
+
+}
---
+++ tests/unit.test.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+
+var lib = process.env.COVERAGE_MODULE_STUDY ? '../lib-cov/' : '../lib/';
+
+describe('study.book',function(){
+    var Isbn  = require(lib + 'isbn');
+    describe('isbn',function(){
+        it('should return data from a ISBN code',function(done){
+            var isbn = new Isbn;
+
+            number = '9780521189064';
+
+            isbn.getBookForUser(number, function(err, data){
+                assert.strictEqual(data[0].title,'English Grammar in Use with Answers');
+                assert.strictEqual(data[0].title,'English Grammar in Use with Answers');
+
+                done();
+            });
+        });
+        it('should return 203 from a ISBN code',function(done){
+            var isbn = new Isbn;
+
+            number = null;
+
+            isbn.getBookForUser(number, function(err, data){
+                assert.strictEqual(err,203);
+                assert.ok(!data);
+
+                done();
+            });
+        });
+    });
+
+});
